d2a8d270f11fb75056080db26799dbed94de2f82
leonardArya <zhiyual.li@aryusa.com>
2014-10-20 15:02:39 -0700

    aaa

---
+++ ProjectAppManager/ReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReminderViewController.h
+//  ProjectAppManager
+//
+//  Created by Leonard Li on 10/20/14.
+//  Copyright (c) 2014 clark Lyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReminderViewController : UIViewController
+
+@end
