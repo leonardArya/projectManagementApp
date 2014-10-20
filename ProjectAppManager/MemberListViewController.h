865c9460dbd545bf3d98536da9ede57430bf188b
lhc069 <lhc069@gmail.com>
2014-10-20 16:09:22 -0700

    hh

---
+++ ProjectAppManager/MemberListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MemberListViewController.h
+//  ProjectAppManager
+//
+//  Created by clark Lyu on 10/20/14.
+//  Copyright (c) 2014 clark Lyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MemberListViewController : UIViewController
+
+@end
---
+++ ProjectAppManager/MemberListViewController.m
@@ -0,0 +1,38 @@
+//
+//  MemberListViewController.m
+//  ProjectAppManager
+//
+//  Created by clark Lyu on 10/20/14.
+//  Copyright (c) 2014 clark Lyu. All rights reserved.
+//
+
+#import "MemberListViewController.h"
+
+@interface MemberListViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *memberListTableView;
+
+@end
+
+@implementation MemberListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
