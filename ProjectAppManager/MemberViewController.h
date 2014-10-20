1954bdd8472edf8c672c41c23e22f7c8acead876
lhc069 <lhc069@gmail.com>
2014-10-20 16:13:37 -0700

    jjj

---
+++ ProjectAppManager/MemberViewController.h
@@ -0,0 +1,13 @@
+//
+//  MemberViewController.h
+//  ProjectAppManager
+//
+//  Created by clark Lyu on 10/20/14.
+//  Copyright (c) 2014 clark Lyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MemberViewController : UIViewController
+
+@end
