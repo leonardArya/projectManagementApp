96f8755d5c791c6bfd2adebdb09f5b475bc88869
leonardArya <zhiyual.li@aryusa.com>
2014-10-27 12:07:36 -0700

    aaa

--- ProjectAppManager/CoreDataManager.m
+++ ProjectAppManager/CoreDataManager.m
@@ -7,7 +7,36 @@
 //
 
 #import "CoreDataManager.h"
+#import <CoreData/CoreData.h>
+#import <UIKit/UIKit.h>
 
 @implementation CoreDataManager
 
+//core data
+- (NSManagedObjectContext *)managedObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
+- (IBAction)save:(id)sender {
+    NSManagedObjectContext *context = [self managedObjectContext];
+    NSNumber * memberid = [NSNumber numberWithInt:2];
+    NSNumber * projectid = [NSNumber numberWithInt:2];
+    // Create a new managed object
+    NSManagedObject *newDevice = [NSEntityDescription insertNewObjectForEntityForName:@"ProjectMemberRelationship" inManagedObjectContext:context];
+    [newDevice setValue:memberid forKey:@"memberID"];
+    [newDevice setValue:projectid forKey:@"projectID"];
+    
+    NSError *error = nil;
+    // Save the object to persistent store
+    if (![context save:&error]) {
+        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+    }
+    
+    //    [self dismissViewControllerAnimated:YES completion:nil];
+}
 @end
--- ProjectAppManager/LoginViewController.m
+++ ProjectAppManager/LoginViewController.m
@@ -47,6 +47,7 @@
         ReminderViewController * reminderViewController = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"reminderView"];
         [self.navigationController pushViewController:reminderViewController animated:YES];
         NSMutableArray * receivedMessages = [[ServerManager sharedInstance] getMessagesByUserID:[UserManager sharedInstance].currentUser.userID];
+        ((Message*)[receivedMessages objectAtIndex:0]);
         NSLog(@"%ld",[receivedMessages count]);
         
     }else{
@@ -58,31 +59,4 @@
 - (IBAction)refreshBtnOnclick:(id)sender {
 }
 
-//core data
-- (NSManagedObjectContext *)managedObjectContext {
-    NSManagedObjectContext *context = nil;
-    id delegate = [[UIApplication sharedApplication] delegate];
-    if ([delegate performSelector:@selector(managedObjectContext)]) {
-        context = [delegate managedObjectContext];
-    }
-return context;
-}
-
-- (IBAction)save:(id)sender {
-    NSManagedObjectContext *context = [self managedObjectContext];
-    NSNumber * memberid = [NSNumber numberWithInt:2];
-    NSNumber * projectid = [NSNumber numberWithInt:2];
-    // Create a new managed object
-    NSManagedObject *newDevice = [NSEntityDescription insertNewObjectForEntityForName:@"ProjectMemberRelationship" inManagedObjectContext:context];
-    [newDevice setValue:memberid forKey:@"memberID"];
-    [newDevice setValue:projectid forKey:@"projectID"];
-    
-    NSError *error = nil;
-    // Save the object to persistent store
-    if (![context save:&error]) {
-        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
-    }
-    
-    //    [self dismissViewControllerAnimated:YES completion:nil];
-}
 @end
